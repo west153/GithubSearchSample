07f149df4a59d5442fbd7b9d4c56a0ebebc9ee45
KimDaseul <west1585@gmail.com>
2020-04-22 12:34:49 +0900

    Redundant code improvement

--- app/src/main/java/com/example/simplegithubsearch/data/source/SearchRepositoryImpl.kt
+++ app/src/main/java/com/example/simplegithubsearch/data/source/SearchRepositoryImpl.kt
@@ -1,31 +1,31 @@
 package com.example.simplegithubsearch.data.source
 
+import com.example.simplegithubsearch.data.User
 import com.example.simplegithubsearch.data.UserDetail
 import com.example.simplegithubsearch.utils.concatMapZip
 import io.reactivex.Flowable
+import io.reactivex.FlowableTransformer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class SearchRepositoryImpl(private val remote: SearchDataSource) : SearchRepository {
   override fun userSearch(user: String): Flowable<List<UserDetail>> {
     return remote.userSearch(user)
-      .map { userList -> userList.map { it.login } }
-      .concatMapZip { userList ->
-        userList.map {
-          remote.getUserDetail(it).subscribeOn(Schedulers.io())
-        }
-      }
-      .subscribeOn(Schedulers.io())
-      .observeOn(AndroidSchedulers.mainThread())
+      .compose(applyDetailSchedulers())
   }
 
   override fun userSearch(user: String, page: Int, per_page: Int): Flowable<List<UserDetail>> {
     return remote.userSearch(user, page, per_page)
-      .map { userList -> userList.map { it.login } }
-      .concatMapZip { userList ->
-        userList.map { remote.getUserDetail(it).subscribeOn(Schedulers.io()) }
-      }
-      .subscribeOn(Schedulers.io())
-      .observeOn(AndroidSchedulers.mainThread())
+      .compose(applyDetailSchedulers())
+  }
+
+  private fun applyDetailSchedulers(): FlowableTransformer<List<User>, List<UserDetail>> {
+    return FlowableTransformer { observable ->
+      observable.map { users -> users.map { it.login } }
+        .map { login -> login.map { remote.getUserDetail(it).subscribeOn(Schedulers.io()) } }
+        .concatMapZip { it }
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+    }
   }
 }
