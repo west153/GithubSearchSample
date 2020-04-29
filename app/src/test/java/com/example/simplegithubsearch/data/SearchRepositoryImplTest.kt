5fdcf8debc83a01b0d6df7a7226e08c2ea52b1f6
KimDaseul <west1585@gmail.com>
2020-04-29 16:34:03 +0900

    search repository impl test code refactoring

--- app/src/test/java/com/example/simplegithubsearch/data/SearchRepositoryImplTest.kt
+++ app/src/test/java/com/example/simplegithubsearch/data/SearchRepositoryImplTest.kt
@@ -1,7 +1,8 @@
 package com.example.simplegithubsearch.data
 
 import com.example.simplegithubsearch.data.source.SearchDataSource
-import com.example.simplegithubsearch.utils.concatMapZip
+import com.example.simplegithubsearch.data.source.SearchRepository
+import com.example.simplegithubsearch.data.source.SearchRepositoryImpl
 import io.reactivex.Flowable
 import org.junit.Before
 import org.junit.Test
@@ -16,57 +17,46 @@ class SearchRepositoryImplTest {
 
   private val user1 = User("user1")
   private val user2 = User("user2")
-  private val userDetail1 = UserDetail("user1", "test1", "test1", "test1")
-  private val userDetail2 = UserDetail("user2", "test2", "test2", "test2")
+  private val userList = arrayListOf(user1, user2)
+
+  private lateinit var repository: SearchRepository
 
   @Mock
   private lateinit var remote: SearchDataSource
 
   @Before
   fun setup() {
-    given(remote.userSearch("user")).willReturn(Flowable.just(arrayListOf(user1, user2)))
-  }
-
-  @Test
-  fun userSearch_hasUser() {
-    //given
-
-    //when
-    val userList = remote.userSearch("user")
-
-    //then
-    userList.test()
-      .assertValue { it.size == 2 }
-  }
-
-  @Test
-  fun userSearch_notFound() {
-    //given
-    given(remote.userSearch(ArgumentMatchers.anyString())).willReturn(Flowable.just(arrayListOf()))
-
-    //when
-    val userList = remote.userSearch("user")
-
-    //then
-    userList.test()
-      .assertValue { it.isEmpty() }
+    repository = SearchRepositoryImpl(remote)
+    given(remote.userSearch("user")).willReturn(Flowable.just(userList))
   }
 
   @Test
   fun userSearch_convertToDetail() {
     //given
+    val userDetail1 = UserDetail("user1", "test1", "test1", "test1")
+    val userDetail2 = UserDetail("user2", "test2", "test2", "test2")
     given(remote.getUserDetail("user1")).willReturn(Flowable.just(userDetail1))
     given(remote.getUserDetail("user2")).willReturn(Flowable.just(userDetail2))
 
     //when
-    val userDetailList = remote.userSearch("user")
-      .map { users -> users.map { it.login } }
-      .map { it.map { login -> remote.getUserDetail(login) } }
-      .concatMapZip { it }
+    val userDetailList = repository.userSearch("user")
 
     //then
     userDetailList.test()
       .assertValue { it[0].bio == "test1" && it[1].bio == "test2" }
+  }
+
+  @Test
+  fun userSearch_NotFoundUsers() {
+    //given
+    given(remote.userSearch(ArgumentMatchers.anyString())).willReturn(Flowable.just(arrayListOf()))
+
+    //when
+    val userList = repository.userSearch("abc")
+
+    //then
+    userList.test()
+      .assertValue { it.isEmpty() }
   }
 
 }
