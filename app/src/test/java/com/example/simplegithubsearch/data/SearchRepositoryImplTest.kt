ad0482914d28384afe584a2cd65dd279b04a711b
KimDaseul <west1585@gmail.com>
2020-04-29 15:28:15 +0900

    search repository impl test code refactoring

--- app/src/test/java/com/example/simplegithubsearch/data/SearchRepositoryImplTest.kt
+++ app/src/test/java/com/example/simplegithubsearch/data/SearchRepositoryImplTest.kt
@@ -1,80 +1,72 @@
 package com.example.simplegithubsearch.data
 
 import com.example.simplegithubsearch.data.source.SearchDataSource
-import com.example.simplegithubsearch.data.source.SearchRepository
-import com.example.simplegithubsearch.data.source.SearchRepositoryImpl
+import com.example.simplegithubsearch.utils.concatMapZip
+import io.reactivex.Flowable
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
+import org.mockito.BDDMockito.given
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class SearchRepositoryImplTest {
-  private lateinit var fakeSearchDataSource: SearchDataSource
-  private lateinit var searchRepository: SearchRepository
+
+  private val user1 = User("user1")
+  private val user2 = User("user2")
+  private val userDetail1 = UserDetail("user1", "test1", "test1", "test1")
+  private val userDetail2 = UserDetail("user2", "test2", "test2", "test2")
+
+  @Mock
+  private lateinit var remote: SearchDataSource
 
   @Before
-  fun createRepository() {
-    fakeSearchDataSource = FakeSearchDataSource()
-    searchRepository =
-      SearchRepositoryImpl(
-        fakeSearchDataSource
-      )
+  fun setup() {
+    given(remote.userSearch("user")).willReturn(Flowable.just(arrayListOf(user1, user2)))
   }
 
   @Test
-  fun userSearch_empty_result() {
+  fun userSearch_hasUser() {
     //given
 
     //when
-    val userList = searchRepository.userSearch("empty")
+    val userList = remote.userSearch("user")
 
     //then
-    userList.test().assertValue { it.isEmpty() }
+    userList.test()
+      .assertValue { it.size == 2 }
   }
 
   @Test
-  fun userSearch_default() {
+  fun userSearch_notFound() {
     //given
-    val users = arrayListOf<User>()
-
-    for (i in 0 until 100) {
-      if (i % 2 == 0)
-        users.add(User("user", "", ""))
-      else
-        users.add(User("test", "", ""))
-    }
-
-    val testDataSource = FakeSearchDataSource(users)
-    val testRepository: SearchRepository =
-      SearchRepositoryImpl(
-        testDataSource
-      )
+    given(remote.userSearch(ArgumentMatchers.anyString())).willReturn(Flowable.just(arrayListOf()))
 
     //when
-    val userList = testRepository.userSearch("user")
+    val userList = remote.userSearch("user")
 
     //then
-    userList.test().assertValue { it.size == 50 }
+    userList.test()
+      .assertValue { it.isEmpty() }
   }
 
   @Test
-  fun userSearch_Paging_FiveUsers() {
+  fun userSearch_convertToDetail() {
     //given
-    val users = arrayListOf<User>()
-
-    for (i in 0 until 100) {
-      users.add(User("user${i + 1}", "", ""))
-    }
-
-    val testDataSource = FakeSearchDataSource(users)
-    val testRepository: SearchRepository =
-      SearchRepositoryImpl(
-        testDataSource
-      )
+    given(remote.getUserDetail("user1")).willReturn(Flowable.just(userDetail1))
+    given(remote.getUserDetail("user2")).willReturn(Flowable.just(userDetail2))
 
     //when
-    val userList = testRepository.userSearch("user", 1, 5)
+    val userDetailList = remote.userSearch("user")
+      .map { users -> users.map { it.login } }
+      .map { it.map { login -> remote.getUserDetail(login) } }
+      .concatMapZip { it }
 
     //then
-    userList.test().assertValue { it.size == 5 }
+    userDetailList.test()
+      .assertValue { it[0].bio == "test1" && it[1].bio == "test2" }
   }
 
 }
